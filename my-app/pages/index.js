717a20dafbf5fdbd2b29257365c4b24fd8d2e3a1
Robert Sneiderman <robbysneiderman@gmail.com>
2024-06-30 19:36:40 -0500

    Moved global CSS import to _app.js

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import '../styles/globals.css';
+// Remove the global CSS import from here
 
 export default function Home() {
   const [selectedFile, setSelectedFile] = useState(null);
