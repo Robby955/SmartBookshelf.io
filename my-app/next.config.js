f3506fb9dc62ac9d3fe5f6cb253ba7745a92b8b5
Robert Sneiderman <robbysneiderman@gmail.com>
2024-07-01 12:23:37 -0500

    Saving current progress before switching to main

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,8 +1,20 @@
 // next.config.js
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   images: {
-    domains: ['localhost', 'new-smartbookshelf-vnbmdiupba-uc.a.run.app'],
+    domains: [
+      'localhost',
+      'new-smartbookshelf-vnbmdiupba-uc.a.run.app',
+      'shelf-value-hd3z9i1jo-robert-s-projects-5f6e9fbd.vercel.app',
+      'shelf-value-io-h5df-grgechind-robert-s-projects-5f6e9fbd.vercel.app'
+    ],
   },
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: isProd
+      ? 'https://new-smartbookshelf-vnbmdiupba-uc.a.run.app/'
+      : 'http://localhost:8000/',
+  },
 };
