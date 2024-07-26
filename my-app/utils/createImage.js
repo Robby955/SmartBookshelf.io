cd3e03825a17873d08a591d4eb33febe9a0c41c4
Robert Sneiderman <robbysneiderman@gmail.com>
2024-07-26 09:42:46 -0500

    Remove ignored files from tracking

--- my-app/utils/createImage.js
+++
@@ -1,9 +0,0 @@
-// utils/createImage.js
-export const createImage = (url) =>
-  new Promise((resolve, reject) => {
-    const image = new Image();
-    image.addEventListener('load', () => resolve(image));
-    image.addEventListener('error', (error) => reject(error));
-    image.setAttribute('crossOrigin', 'anonymous'); // to avoid CORS issues
-    image.src = url;
-  });
--- my-app/utils/getRadianAngle.js
+++
@@ -1,4 +0,0 @@
-// utils/getRadianAngle.js
-export const getRadianAngle = (degreeValue) => {
-  return (degreeValue * Math.PI) / 180;
-};
